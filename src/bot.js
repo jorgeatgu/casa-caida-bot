28c01be2c32d314d793f3a15adaf110fa7d29dc6
Jorge Aznar <jorgeatgu@gmail.com>
2019-04-03 23:53:36 +0200

    post with image

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,39 @@
 const Twit = require('twit');
 const config = require('./config');
-const data = require('../data/aragon-municipios-densidad-inferior.json');
+const fs = require('fs');
+const datos = require('../data/aragon-municipios-densidad-inferior.json');
 const bot = new Twit(config);
 
-for (let i = 1; i <= data.length; i++) {
+for (let i = 1; i <= datos.length; i++) {
     (function(index) {
         setTimeout(function() {
-            let status = `${data[i].municipio} en el año 1900 tenía ${
-                data[i].milnovecientos
+            let estado = `${datos[i].municipio} en el año 1900 tenía ${
+                datos[i].milnovecientos
             } habitantes, en 2018 tiene ${
-                data[i].dosmil
-            } habitantes. Ha perdido el`;
-            bot.post(
-                'statuses/update',
-                {
-                    status
-                },
-                (err, response, data) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Post success!');
-                    }
-                }
+                datos[i].dosmil
+            } habitantes. Ha perdido el #${datos[i].pronvicia}`;
+            let b64content = fs.readFileSync(
+                `images/buenas/${datos[i].foto}.jpg`,
+                { encoding: 'base64' }
             );
-        }, i * 60000);
+            bot.post('media/upload', { media_data: b64content }, function (err, data, response) {
+              // now we can assign alt text to the media, for use by screen readers and
+              // other text-based presentations and interpreters
+              let mediaIdStr = data.media_id_string
+              let altText = `Gráfica sobre la despoblación en ${datos[i].municipio} entre los años 1900 y 2018.`
+              let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+              bot.post('media/metadata/create', meta_params, function (err, data, response) {
+                if (!err) {
+                  // now we can reference the media and post a tweet (media will attach to the tweet)
+                  let params = { status: estado, media_ids: [mediaIdStr] }
+
+                  bot.post('statuses/update', params, function (err, data, response) {
+                    console.log(data)
+                  })
+                }
+              })
+            })
+        }, i * 6000);
     })(i);
 }
