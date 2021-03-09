14089ea8d246f46763e54644a401e7fb1a957b99
Jorge <jorgeatgu@gmail.com>
2021-03-09 21:33:58 +0100

    update bot

--- src/bot.js
+++ src/bot.js
@@ -1,65 +1,81 @@
 const Twit = require('twit');
 const config = require('./config');
 const fs = require('fs');
-const datos = require('../data/aragon-municipios-densidad-inferior.json');
+const datos = require('../data/tweet02.json');
 const bot = new Twit(config);
+const stream = bot.stream('statuses/filter', { track: '@casacaida' });
 
-for (let i = 0; i <= datos.length; i++) {
-    (function(index) {
-        setTimeout(function() {
-            let estado = `${datos[i].municipio} en la provincia de #${
-                datos[i].provincia
-            }.
-En el año 1900 tenía ${datos[i].milnovecientos} habitantes.
-En el año 2018 tenía ${
-                datos[i].dosmil
-            } habitantes.
-Ha perdido el ${
-                datos[i].porcentaje
-            }% de sus habitantes.
-#despoblacion #EspañaVaciada`;
-            let b64content = fs.readFileSync(
-                `images/buenas/${datos[i].foto}.jpg`,
-                { encoding: 'base64' }
-            );
-            bot.post('media/upload', { media_data: b64content }, function(
-                err,
-                data,
-                response
-            ) {
-                // now we can assign alt text to the media, for use by screen readers and
-                // other text-based presentations and interpreters
-                let mediaIdStr = data.media_id_string;
-                let altText = `Gráfica sobre la despoblación en ${
-                    datos[i].municipio
-                } entre los años 1900 y 2018.`;
-                let meta_params = {
-                    media_id: mediaIdStr,
-                   alt_text: { text: altText }
+stream.on('tweet', parseTweet)
+
+function parseTweet(message) {
+  const {
+    in_reply_to_screen_name: replyTo,
+    text: text,
+    user: {
+      screen_name: userName
+    }
+  } = message
+  const nameOfTheMunicipality = text.replace(/@casacaida /g,'');
+
+  if (replyTo === 'casacaida') {
+    createImagesTweet(nameOfTheMunicipality, userName)
+  }
+}
+
+function createImagesTweet(nameOfTheMunicipality, userName) {
+  const parseNameOfTheMunicipality = nameOfTheMunicipality.toLowerCase();
+
+  const charts = ['densidad','evolucion', 'evolucion2010', 'habitantes']
+
+  let stringImages = []
+
+  for(let item of charts) {
+    const b64content = fs.readFileSync(
+      `images/${item}/${parseNameOfTheMunicipality}.jpg`, { encoding: 'base64' }
+    );
+    bot.post('media/upload', { media_data: b64content }, function(
+      err,
+      data,
+      response
+    ) {
+      const mediaId = data.media_id_string;
+      stringImages.push(mediaId);
+      if(stringImages.length === 4) {
+        let media_ids_string = []
+        for (let index = 0; index < stringImages.length; index++) {
+          let meta_params = {
+            media_id: stringImages[index]
+          };
+          bot.post('media/metadata/create', meta_params, function(
+            err,
+            data,
+            response
+          ) {
+            if (!err) {
+              media_ids_string.push(meta_params.media_id)
+              if (media_ids_string.length === 4) {
+                const params = {
+                  status: `@${userName} Aquí tienes cuatro gráficas de ${nameOfTheMunicipality} #Aragón`,
+                  media_ids: media_ids_string
                 };
+                createTweet(params)
+              }
+            } else {
+              console.log(err)
+            }
+          });
+        }
+      }
+    });
+  }
+}
 
-                bot.post('media/metadata/create', meta_params, function(
-                    err,
-                    data,
-                    response
-                ) {
-                    if (!err) {
-                        // now we can reference the media and post a tweet (media will attach to the tweet)
-                        let params = {
-                            status: estado,
-                            media_ids: [mediaIdStr]
-                        };
-
-                        bot.post('statuses/update', params, function(
-                            err,
-                            data,
-                            response
-                        ) {
-                            console.log(data);
-                        });
-                    }
-                });
-            });
-        }, i * 3600000);
-    })(i);
+function createTweet(params) {
+  bot.post('statuses/update', params, function(
+    err,
+    data,
+    response
+  ) {
+    console.log(data);
+  });
 }
